21f4e93c8536c144f3da7d0cb68a81e4fe472693
notdylandover <main.ddover@gmail.com>
2024-10-12 01:09:47 -0500

    revert changes to index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, useLocation } from 'react-router-dom';
-import React, { useEffect } from 'react';
-import * as ReactDOM from 'react-dom/client';
+
+import React from 'react';
+import * as ReactDOM from "react-dom/client";
 import './styles/index.css';
 import routes from './routes';
 import Sidebar from './components/sidebar';
@@ -8,34 +9,10 @@ import Sidebar from './components/sidebar';
 function App() {
     const location = useLocation();
 
-    const storeVisitedPage = (path) => {
-        const visitedPages = JSON.parse(localStorage.getItem('visitedPages')) || [];
-        if (!visitedPages.includes(path)) {
-            visitedPages.push(path);
-            localStorage.setItem('visitedPages', JSON.stringify(visitedPages));
-        }
-    };
-
-    useEffect(() => {
-        storeVisitedPage(location.pathname);
-    }, [location.pathname]);
-
-    const getVisitedPages = () => {
-        return JSON.parse(localStorage.getItem('visitedPages')) || [];
-    };
-
     return (
         <div>
             {routes}
             {location.pathname !== '/' && <Sidebar />}
-            <div>
-                <h2>Visited Pages:</h2>
-                <ul>
-                    {getVisitedPages().map((page, index) => (
-                        <li key={index}>{page}</li>
-                    ))}
-                </ul>
-            </div>
         </div>
     );
 }
