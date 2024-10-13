d98f62062dcffae4efea9713b01c351e958afb48
Nick <nhowell3@atu.edu>
2024-10-13 16:54:24 -0500

    WeatherApiFetched

--- frontend/src/pages/WeatherPage.js
+++ frontend/src/pages/WeatherPage.js
@@ -1,13 +1,51 @@
 import React from 'react';
 import Widget from '../components/homeWidget';
 import GenericPage from '../components/genericPage';
+import WeatherAPI from '../scripts/weather';
+import { useState, useEffect } from 'react';
 
 export default function WeatherPage() {
+    const [data, setData] = useState(null)
+
+    useEffect(() => {
+        WeatherAPI().then(x => {
+            setData(x);
+        });
+    }, [])
+
     return (
         <>
             {new GenericPage(
                 <>
-                    {new Widget(<p className="text-xl rounded-full w-full h-full shadow-[0_0_0.5vh_rgba(0,0,0,0.5)] p-4">Weather</p>)}
+                {data &&
+                <>
+                    <div className="flex space-x-4">
+
+                    
+                        <div className="flex-fit">
+                            {new Widget(<p className="text-xl rounded-full h-full shadow-[0_0_0.5vh_rgba(0,0,0,0.5)] p-4">back</p>)}
+                        </div>
+                        <div className="w-full">
+                            {new Widget(
+                                <p className="text-xl rounded-full w-auto h-full shadow-[0_0_0.5vh_rgba(0,0,0,0.5)] p-4">
+                                    {data.periods[0].temperature} F    {data.periods[0].shortForecast}
+                                </p>)}
+                        </div>
+                    </div>
+                        {new Widget(
+                            <ul>
+                                {data.periods.map(period => {
+                                    return (
+                                        <li>{period.name} {period.temperature} {period.startTime} {period.endTime} {period.shortForecast}</li>
+                                    );
+                                })}
+                            </ul>
+                        )}
+                        {new Widget(
+                            <img src={data.radar}></img>
+                        )}
+                </>
+                }
                 </>
             )}
         </>
--- frontend/src/scripts/weather.js
+++ frontend/src/scripts/weather.js
@@ -1,1 +1,37 @@
-// Get the weather from Weather.GOV API
+async function getAPIjson(url){
+    try{
+        // Get weather info
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+
+        const json = await response.json();
+
+        return json;
+    } catch (error){
+        console.error(error.message);
+        return null
+    }
+}
+
+export default async function WeatherAPI(){
+    const url = 'https://api.weather.gov/points/35.2938,-93.1361';
+
+    let weatherData = {};
+
+    let response = await getAPIjson(url);
+
+    weatherData.periods = await getAPIjson(response.properties.forecast).then(x => {
+        return x.properties.periods;
+    });
+
+    let radarUrl = `https://radar.weather.gov/ridge/standard/${response.properties.radarStation}_loop.gif`;
+
+    // CORS error here, need backend fix
+    //weatherData.radar = await fetch(radarUrl);
+
+    console.log(response);
+
+    return weatherData;
+}
