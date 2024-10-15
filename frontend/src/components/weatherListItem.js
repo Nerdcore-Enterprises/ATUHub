57ff75598b9188996501332d7574e44d53b5e63e
Nick <nhowell3@atu.edu>
2024-10-15 13:48:35 -0500

    WeatherStyled

--- frontend/src/components/weatherListItem.js
+++ frontend/src/components/weatherListItem.js
@@ -1,12 +1,31 @@
+import cloud from '../assets/icons/weather/cloud.svg' 
+// import cloudSun from '../assets/icons/weather/cloud-sun.svg' 
+import rain from '../assets/icons/weather/rain.svg' 
+import sun from '../assets/icons/weather/sun.svg' 
+import thunderstorm from '../assets/icons/weather/thunderstorm.svg' 
+
 export default function WeatherListItem(name, high, low, shortForecast){
+    let icon = null;
+
+    if (shortForecast.toLowerCase().includes("sun")) icon = sun;
+    if (shortForecast.toLowerCase().includes("rain")) icon = rain;
+    if (shortForecast.toLowerCase().includes("cloud")) icon = cloud;
+    if (shortForecast.toLowerCase().includes("thunder")) icon = thunderstorm;
+
     return(
         <li className="p-2.5 w-full min-w-min">
             <div className="w-full text-center text-nowrap">
                 {name}
             </div>
-            <div className="w-full text-center text-nowrap">
-                {shortForecast}
-            </div>
+            {icon ?(
+                <div className="w-full text-center text-nowrap">
+                    <img src={icon} className='m-auto min-w-7' alt='weather-icon'></img>
+                </div>
+            ) : (
+                <div className="w-full text-center text-nowrap">
+                    {shortForecast}
+                </div>
+            )}
             <div className="w-full text-center text-nowrap">
                 {high} / {low}
             </div>
--- frontend/src/pages/WeatherPage.js
+++ frontend/src/pages/WeatherPage.js
@@ -3,17 +3,33 @@ import Widget from '../components/homeWidget';
 import GenericPage from '../components/genericPage';
 import WeatherAPI from '../scripts/weather';
 import WeatherListItem from '../components/weatherListItem';
+import { useNavigate } from 'react-router-dom';
 import { useState, useEffect } from 'react';
+import cloud from '../assets/icons/weather/cloud.svg' 
+// import cloudSun from '../assets/icons/weather/cloud-sun.svg' 
+import rain from '../assets/icons/weather/rain.svg' 
+import sun from '../assets/icons/weather/sun.svg' 
+import thunderstorm from '../assets/icons/weather/thunderstorm.svg'  
 import '../styles/weather.css'
 
 export default function WeatherPage() {
-    const [data, setData] = useState(null)
+    const [data, setData] = useState(null);
+    const [icon, setIcon] = useState(null);
+    const navigate = useNavigate();
 
     useEffect(() => {
         WeatherAPI().then(x => {
             setData(x);
+            if (x.periods[0].shortForecast.toLowerCase().includes("sun")) setIcon(sun);
+            if (x.periods[0].shortForecast.toLowerCase().includes("rain")) setIcon(rain);
+            if (x.periods[0].shortForecast.toLowerCase().includes("cloud")) setIcon(cloud);
+            if (x.periods[0].shortForecast.toLowerCase().includes("thunder")) setIcon(thunderstorm);
         });
     }, [])
+
+    const handleNav = (path) => {
+        navigate(path);
+    };
 
     return (
         <>
@@ -24,14 +40,21 @@ export default function WeatherPage() {
                     <div className="flex space-x-4">
 
                     
-                        <div className="flex-fit">
-                            {new Widget(<p className="text-xl rounded-full h-full shadow-[0_0_0.5vh_rgba(0,0,0,0.5)] p-4">back</p>)}
+                        <div className="flex-fit h-full">
+                            <button onClick={() => handleNav('../home')}>
+                                {new Widget(<p className="text-xl rounded-full h-full  p-4 font-extrabold pl-8 pr-8">&lt;</p>)}
+                            </button>
                         </div>
                         <div className="w-full">
                             {new Widget(
-                                <p className="text-xl rounded-full w-auto h-full shadow-[0_0_0.5vh_rgba(0,0,0,0.5)] p-4">
-                                    {data.periods[0].temperature} F    {data.periods[0].shortForecast}
-                                </p>)}
+                                <>
+                                    <ul className='inline-flex w-full pl-10 pr-10'>
+                                        <li className='pt-1 pb-1 w-full min-w-min content-center h-full'><img src={icon} className='min-w-10 align-middle pt-1' alt='weather-icon'></img></li>
+                                        <li className='pt-1 pb-1 w-full min-w-min'><h1>{data.periods[0].shortForecast}</h1></li>
+                                        <li className='pt-1 pb-1 w-full min-w-min text-right'><h1>{data.periods[0].temperature} F</h1></li>
+                                    </ul>
+                                </>
+                                )}
                         </div>
                     </div>
                         {new Widget(
@@ -52,7 +75,7 @@ export default function WeatherPage() {
                         , "p-4")}
                         {new Widget(
                             <>
-                                <img src={data.radar}></img>
+                                <img src={data.radar} alt='weather-radar'></img>
                                 radar goes here LOL
                             </>
                         )}
