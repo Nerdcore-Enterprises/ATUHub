94189ab8951693801fba0a3fcea0acd5aa842978
Nick <143543314+nhowell3@users.noreply.github.com>
2025-01-22 15:53:38 -0600

    Initial Job Widget

---
+++ frontend/src/components/JobSearch/JobWidget.js
@@ -0,0 +1,49 @@
+import { useState, useEffect } from "react";
+import Widget from "../homeWidget";
+
+export default function JobWidget({jobData}){
+    const [tagData, setTagData] = useState([]);
+
+    const dateDifference = () => {
+        const currentDate = new Date();
+
+        const diffInMs = currentDate.getTime() - jobData.postedTime.getTime();
+        const msPerDay = 1000 * 60 * 60 * 24;
+        return Math.floor(diffInMs / msPerDay);
+    }
+
+    const fetchTags = () => {
+        let output = [];
+        output.push("$" + jobData.pay + " per hour");
+        output.push(jobData.time);
+        return output;
+    }
+
+    useEffect(() => {
+        setTagData(fetchTags());
+    }, []);
+
+    return(
+        <Widget>
+            <div className="my-4 mx-6">
+                <p className="text-3xl font-semibold mb-3">
+                    {jobData.name}
+                </p>
+                <p>{jobData.address}</p>
+                <div className="flex space-x-4 rounded-b-[1rem] overflow-x-auto weather-scroll">
+                    {tagData.map((tag, index) => (
+                        <div key={index} className="bg-[#333333] text-white rounded-[2rem] flex flex-col items-center2 pr-4 pl-4 pt-2 pb-2">
+                            <p>{tag}</p>
+                        </div>
+                    ))}
+                </div>
+                    <div className="flex flex-row">
+                        <p className="align-middle w-1/2 mt-auto mb-auto">Posted {dateDifference()} days ago</p>
+                        <button onClick={() => {}} className="bg-[var(--ATUGreen)] ml-auto w-1/2 rounded-[1.5rem] text-white font-semibold py-3 px-6 shadow-[0_0_0.5vh_rgba(0,0,0,0.5)]">
+                            View
+                        </button>
+                    </div>
+            </div>
+        </Widget>
+    );
+}
--- frontend/src/pages/JobsPage.js
+++ frontend/src/pages/JobsPage.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import Header from '../components/header';
 import GenericPage from '../components/genericPage';
+import JobWidget from '../components/JobSearch/JobWidget';
 
 export default function JobsPage() {
+    const testJobData = {
+        name: "Slims Chickens",
+        address: "404 Nowhere St, 12345",
+        pay: 99,
+        time: "Full Time",
+        postedTime: new Date("2025-01-01"),
+    }
+
     return (
         <GenericPage>
             <Header title="Jobs" />
+            <JobWidget jobData={testJobData}/>
         </GenericPage>
     );
 }
